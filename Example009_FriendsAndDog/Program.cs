6d02e9c36a4095e3e2a4388abcf8bb9d967d4b9f
Krasikov Aleksandr <3511667@gmail.com>
2023-02-09 23:32:54 +0300

    Задача про друзей и собаку навстречу друг к другу

---
+++ Example009_FriendsAndDog/Program.cs
@@ -0,0 +1,23 @@
+﻿int FirstFriendsSpeed = 1;
+int SecondFriendsSpeed = 2;
+int DogSpeed = 5;
+int count = 0;
+int Distance = 10000;
+int time = 0;
+int Friend = 2;
+while(Distance >10)
+{
+    if (Friend == 1)
+    {
+        time = Distance / (FirstFriendsSpeed + DogSpeed);
+        Friend = 2;
+    }
+    else
+    {
+        time = Distance / (SecondFriendsSpeed + DogSpeed);
+        Friend = 1;
+    }
+    Distance = Distance - ((FirstFriendsSpeed + SecondFriendsSpeed) * time);
+    count = count + 1;
+}
+Console.WriteLine(count);
