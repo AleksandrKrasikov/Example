a074a2fdff98f1b0c06174e897813e4f4389bbca
Krasikov Aleksandr <3511667@gmail.com>
2023-02-12 00:37:14 +0300

    Lesson1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Умножаем число само на себя.
+
+
+int number;
+Console.Write("Введите число: ");
+//string text = Console.ReadLine()!;
+
+int.TryParse(Console.ReadLine()!, out number);
+//int number = int.Parse(text);
+
+Console.Write($"{number}^2 = {number * number}"); 
+/*Console.Write(number);
+Console.Write("^2 = ");
+Console.Write(number * number);*/
