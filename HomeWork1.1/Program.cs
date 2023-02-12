9d3913890ebf26076c2efa19834fbad30e066855
Krasikov Aleksandr <3511667@gmail.com>
2023-02-12 20:33:29 +0300

    HomeWork1.2

--- HomeWork1.1/Program.cs
+++ HomeWork1.1/Program.cs
@@ -4,14 +4,13 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3        */
 
-int a; 
-int b;
+int a, b;
 Console.Write("Введите число: ");
 int.TryParse(Console.ReadLine()!, out a);
 Console.Write("Введите число: ");
 int.TryParse(Console.ReadLine()!, out b);
 if (a > b)
-    Console.Write($" max = {a}");
+    Console.Write($" max = {a} min = {b}");
 
 else
-    Console.Write($" max = {b}");
+    Console.Write($" max = {b} min = {a}");
