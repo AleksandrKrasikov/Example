12d1ea3b29e09475a9ae9028b06076c0f33d4f8f
Krasikov Aleksandr <3511667@gmail.com>
2023-02-12 13:29:39 +0300

    Lesson 1.3

--- Lesson1.2/Program.cs
+++ Lesson1.2/Program.cs
@@ -8,9 +8,7 @@ Console.Write("Введите число: ");
 int.TryParse(Console.ReadLine()!, out number);
 
 if (number < 1 || number > 7)
-{
     Console.Write("Нет такого дня недели");
-}
 
 /*if (number > 7)
 {
@@ -22,30 +20,16 @@ if (number < 1)
 }*/
 
 if (number == 1)
-{
     Console.Write("Понедельник");
-}
 else if (number == 2)
-{
     Console.Write("Вторник");
-}
 else if (number == 3)
-{
     Console.Write("Среда");
-}
 else if (number == 4)
-{
     Console.Write("Четверг");
-}
 else if (number == 5)
-{
     Console.Write("Пятница");
-}
 else if (number == 6)
-{
     Console.Write("Суббота");
-}
 else if (number == 7)
-{
     Console.Write("Воскресенье");
-}
