4d016226d58c79909d58d9aaac65f31a0f54299e
Krasikov Aleksandr <3511667@gmail.com>
2023-02-27 02:07:43 +0300

    HomeWork1.16

---
+++ HomeWork1.16/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+double[] array = new double [5];
+FillArray(array);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+
+Console.Write($"[{String.Join("  ", array)}] - > {max - min}");
+
+void FillArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(new Random().Next(0, 100));
+        }
+}
